079a185cb246dc5d4dfc6f6cb0fb44c72a909102
Oscar Paucara <oscar.paucara@thefanleague.com>
2014-06-09 15:14:36 -0300

    adding basic settings

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+
+  // Configuration goes here
+  grunt.initConfig({
+
+  });
+
+  // Load plugins here
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Define your tasks here
+//  grunt.registerTask('default', ['coffee', 'copy', 'jshint', 'compress']);
+
+};
