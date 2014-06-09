b14a26f9b1dddf2c9ff72678d026bfb81060df9f
Oscar Paucara <oscar.paucara@thefanleague.com>
2014-06-09 16:15:35 -0300

    Definition of uglify to generate all in one

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,25 @@ module.exports = function(grunt) {
 
   // Configuration goes here
   grunt.initConfig({
-
+    uglify: {
+      js: {
+        files: { 'js/min/all.js': [
+          // using whichever order of importance you need
+          'js/*.js'
+        ]}
+      }
+    }
   });
 
   // Load plugins here
   grunt.loadNpmTasks('grunt-contrib-uglify');
   // Define your tasks here
-//  grunt.registerTask('default', ['coffee', 'copy', 'jshint', 'compress']);
+  //  grunt.registerTask('default', ['coffee', 'copy', 'jshint', 'compress']);
 
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+  // Uglify Tasks
+  //grunt.registerTask('customers', ['uglify:customers']);
+  // LESS Tasks
+  //grunt.registerTask('customers_less', ['less:customers_less']);
 };
